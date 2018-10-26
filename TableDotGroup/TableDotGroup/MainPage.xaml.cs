e510bfedd2c1478a8352384eb96f1c3fff6154a2
Leo383 <44499435+Leo383@users.noreply.github.com>
2018-10-26 14:33:48 +0200

    Add project files.

---
+++ TableDotGroup/TableDotGroup/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TableDotGroup
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+            logo.Source = ImageSource.FromResource("TableDotGroup.Images.Minka.jpg");
+
+           
+        }
+
+        async void Button_Clicked(object sender, EventArgs e)
+        {                  
+            await Navigation.PushAsync(new Schüler());
+            
+        }
+    }
+}
---
+++ TableDotGroup/TableDotGroup/Tischgruppen.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TableDotGroup
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Tischgruppen : ContentPage
+	{
+		public Tischgruppen ()
+		{
+			InitializeComponent ();
+            double t3 = 
+		}
+
+        private void Stepper_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            
+        }
+    }
+}
