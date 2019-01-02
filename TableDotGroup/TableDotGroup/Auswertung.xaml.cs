58df7e7ac2f5509808405d76a94aeacaafd08922
Leo383 <44499435+Leo383@users.noreply.github.com>
2019-01-02 18:23:56 +0100

    Backup

--- TableDotGroup/TableDotGroup/Auswertung.xaml.cs
+++ TableDotGroup/TableDotGroup/Auswertung.xaml.cs
@@ -9,34 +9,95 @@ using Xamarin.Forms.Xaml;
 
 namespace TableDotGroup
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class Auswertung : ContentPage
-	{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Auswertung : ContentPage
+    {
         static Random random = new Random();
         public double freieSitze;
         public int m;
         public int f;
+        public double dreierTischFrei;
+
+
+        public class TischDrei
+        {
+            public string student;
+        }
+
+        public class TischVier
+        {
+            public string student;
+        }
+
+        public class TischFuenf
+        {
+            public string student;
+        }
+
+        public class TischSechs
+        {
+            public string student;
+        }
+
+        
 		public Auswertung (List<string> namesGirls, List<string> namesBoys, double tischgruppenDreier, double tischgruppenVierer, double tischgruppenFuenfer, double tischgruppenSechser, double freieSitze)
 		{
 			InitializeComponent ();
 
-            print.Text = freieSitze.ToString();
-            namesBoys[1].ToString();
 
 
-            
-            m = random.Next(0, namesBoys.Count());
-            f = random.Next(0, namesGirls.Count());
-            
-            
-            while(namesBoys.Count() > 0)
+            for (int i = 0; i < tischgruppenDreier; i++)    //Group of 3
             {
-                var picked = namesBoys[random.Next(0, namesBoys.Count())];
-                namesBoys.Remove(picked);
 
-                print.Text = picked;
+                for (int j = 0; j < 4; j++)
+                {
+                    var picked = namesBoys[random.Next(0, namesBoys.Count())];
+                    namesBoys.Remove(picked);
+                    picked = new TischDrei().student;
+
+                }
             }
-            
-		}
+
+            for (int i = 0; i < tischgruppenVierer; i++) //for each group of 4
+            {
+
+                for (int j = 0; j < 4/2; j++) //only do this two times because 50% boys
+                {
+                    var picked = namesBoys[random.Next(0, namesBoys.Count())]; 
+                    namesBoys.Remove(picked);
+                    List<string> alleTische = new List<TischVier>();
+                    TischVier t = new TischVier();
+                    t.student = picked;
+                    alleTische.Add(t.student);
+
+                }
+            }
+
+            for (int i = 0; i < tischgruppenFuenfer; i++) //Fuenfer Tisch
+            {
+
+                for (int j = 0; j < 4; j++)
+                {
+                    var picked = namesBoys[random.Next(0, namesBoys.Count())];
+                    namesBoys.Remove(picked); 
+                    picked = new TischFuenf().student;
+
+                }
+            }
+
+            for (int i = 0; i < tischgruppenSechser; i++) // Sechser TIsch
+            {
+
+                for (int j = 0; j < 4; j++)
+                {
+                    var picked = namesBoys[random.Next(0, namesBoys.Count())];
+                    namesBoys.Remove(picked);
+                    picked = new TischSechs().student;
+
+                }
+            }
+
+
+        }
 	}
 }
